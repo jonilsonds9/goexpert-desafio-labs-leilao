0c4f2b2890db859add204ff09f9a92e636b35f91
Jonilson Sousa <jonilsonds9@gmail.com>
2025-10-30 15:58:10 -0300

    Adicionando fechamento automatico de leiloes

--- internal/infra/database/auction/create_auction.go
+++ internal/infra/database/auction/create_auction.go
@@ -2,9 +2,13 @@ package auction
 
 import (
 	"context"
+	"os"
+	"time"
+
 	"github.com/jonilsonds9/goexpert-desafio-labs-leilao/configuration/logger"
 	"github.com/jonilsonds9/goexpert-desafio-labs-leilao/internal/entity/auction_entity"
 	"github.com/jonilsonds9/goexpert-desafio-labs-leilao/internal/internal_error"
+	"go.mongodb.org/mongo-driver/bson"
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -46,5 +50,31 @@ func (ar *AuctionRepository) CreateAuction(
 		return internal_error.NewInternalServerError("Error trying to insert auction")
 	}
 
+	go func() {
+		ctx := context.Background()
+
+		select {
+		case <-time.After(getAuctionInterval()):
+			update := bson.M{"$set": bson.M{"status": auction_entity.Completed}}
+			filter := bson.M{"_id": auctionEntityMongo.Id}
+
+			_, err := ar.Collection.UpdateOne(ctx, filter, update)
+			if err != nil {
+				logger.Error("Error trying to update auction status to completed", err)
+				return
+			}
+			logger.Info("Auction completed automatically")
+		}
+	}()
+
 	return nil
+}
+
+func getAuctionInterval() time.Duration {
+	auctionInterval := os.Getenv("AUCTION_INTERVAL")
+	duration, err := time.ParseDuration(auctionInterval)
+	if err != nil {
+		return time.Minute * 5
+	}
+	return duration
 }
